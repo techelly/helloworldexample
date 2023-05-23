c93bbee4159132f70366d10c4de974897476f387
prabhat1510 <prabhatc@gmail.com>
2023-05-23 09:27:19 +0530

    first commit

---
+++ src/main/java/com/helloworldexample/HelloworldexampleApplication.java
@@ -0,0 +1,13 @@
+package com.helloworldexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworldexample/HelloworldexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworldexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
